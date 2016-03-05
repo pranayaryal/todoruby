3d73206719215973d6642553699ff4bf78686429
pranayaryal <drpranayaryal@gmail.com>
2016-03-05 02:00:38 -0500

    rspec for todo items create

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,4 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+  validates :content, length: { minimum: 3}, presence: true
 end
--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -19,6 +19,31 @@ describe "Adding todo items" do
     within("ul.todo_items") do
       expect(page).to have_content("Milk")
     end
-
   end
+
+  it "displays an error when the todo item has no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo item"
+    expect(page).to have_content("New Todo Item")
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
+    end
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  it "displays an error with content less than 3 characters" do
+    visit_todo_list(todo_list)
+    click_link "New Todo item"
+    expect(page).to have_content("New Todo Item")
+    fill_in "Content", with: "as"
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
+    end
+    expect(page).to have_content("Content is too short")
+  end
+
+
 end
